d2f855abe023136e7b309aa7c6f9c66692ac6a7a
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-08-11 20:43:38 +0100

    added user schema

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const {Schema,model}=require("mongoose")
+
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true, 
+            unique: true,
+            trim: true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            match:[/.+@.+\..+/,"must match an email string"],
+
+        },
+        thoughts:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Thought",
+
+            },
+
+            
+        ],
+
+      friends:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        },
+      ],
+
+        
+    },
+    {
+        toJSON:{
+            virtuals:true,
+        },
+        id:false,
+
+    }
+);
+userSchema.virtual("friendCount").get(()=>{
+    return this.friends.length
+})
+const User=model("User",userSchema)
+module.exports= User
