6e92b184cd5e94f8228df5bc2b6bc96ced5c0a6d
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-08-14 23:42:13 +0100

    fixed error

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express= require ("express")
 const DB=require("./config/connection")
+const routes=require("./routes")
 const PORT=process.env.PORT||3002
 const app=express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(routes)
 DB.once("open",()=>{
     app.listen(PORT,()=>{
         console.log(`my server is running on port ${PORT}`)
